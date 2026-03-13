fc50ad51e2ab1875029f9d637563136ab51f58a2
kuziechambers <kuzie.chambers@gmail.com>
2026-03-12 22:45:39 -0500

    add tf files

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,6 +1,19 @@
+resource "aws_ecr_repository" "parking_permit_bot_ecr" {
+  name                 = "${var.lambda_name}-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    ManagedBy = "terraform"
+  }
+}
+
 resource "aws_lambda_function" "parking_permit_bot_lambda" {
-  function_name = "parking-permit-bot"
+  function_name = var.lambda_name
   role          = aws_iam_role.lambda_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.parking_permit_bot.repository_url}:latest"
+  timeout       = 300
+  memory_size   = 1024
 
   tags = {
     ManagedBy = "terraform"
